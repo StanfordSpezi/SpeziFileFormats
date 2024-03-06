e8aee5fcb9f5be7e7b4b8dedf670df93af15e4b4
Andreas Bauer <andreas.bauer@stanford.edu>
2024-03-05 21:13:11 -0800

    Add more tests

---
+++ Tests/EDFFormatTests/Defaults.swift
@@ -0,0 +1,87 @@
+//
+// This source file is part of the Stanford Spezi open-source project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import EDFFormat
+import Foundation
+import XCTest
+
+
+extension URL {
+    static func createTmpFile(name: String) throws -> URL {
+        let tmpDir = NSTemporaryDirectory()
+
+        let url = try XCTUnwrap(NSURL.fileURL(withPathComponents: [tmpDir, name]))
+        if FileManager.default.fileExists(atPath: url.path) {
+            try FileManager.default.removeItem(at: url)
+        }
+        FileManager.default.createFile(atPath: url.path, contents: nil)
+
+        return url
+    }
+}
+
+
+extension Date {
+    static func createDate(
+        year: Int? = nil,
+        month: Int? = nil,
+        day: Int? = nil,
+        hour: Int? = nil,
+        minute: Int? = nil,
+        second: Int? = nil
+    ) throws -> Date {
+        var components = DateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        components.hour = hour
+        components.minute = minute
+        components.second = second
+
+        let date = Calendar.current.date(from: components)
+        return try XCTUnwrap(date)
+    }
+}
+
+
+extension FileInformation {
+    static func testFile(start startDate: Date, birthdate: Date) -> FileInformation {
+        FileInformation(
+            subject: .structured(PatientInformation( // TODO: test anonymsous!
+                code: "P-1",
+                sex: .male,
+                birthdate: birthdate,
+                name: "Test Patient" // tests escaping!
+            )),
+            recording: .structured(RecordingInformation(
+                startDate: startDate,
+                code: "R-1",
+                investigatorCode: "I-1",
+                equipmentCode: "Biopot"
+            )),
+            recordDuration: 1
+        )
+    }
+}
+
+
+extension Signal {
+    static var testEEGSignal: Signal {
+        Signal(
+            label: .eeg(location: .af8, prefix: .micro),
+            transducerType: "test-transducer",
+            prefiltering: "test-perfiltering",
+            sampleCount: 5,
+            physicalMinimum: -200,
+            physicalMaximum: 200,
+            digitalMinimum: -250,
+            digitalMaximum: 250,
+            reserved: "This is reserved."
+        )
+    }
+}
--- Tests/EDFFormatTests/EDFEncodingTests.swift
+++ Tests/EDFFormatTests/EDFEncodingTests.swift
@@ -15,86 +15,27 @@ import XCTest
 // TODO: import XCTByteCoding?
 
 final class EDFEncodingTests: XCTestCase {
-    private lazy var testDate: Date? = {
-        var components = DateComponents()
-        components.year = 2024
-        components.month = 3
-        components.day = 4
-        components.hour = 18
-        components.minute = 51
-        components.second = 10
-
-        return Calendar.current.date(from: components)
-    }()
-
-    private lazy var birthDate: Date? = {
-        var components = DateComponents()
-        components.year = 1998
-        components.month = 5
-        components.day = 26
-
-        return Calendar.current.date(from: components)
-    }()
-
-    func testEDFHeaderEncoding() throws {
-        // swiftlint:disable:previous function_body_length
-
-        let tmpDir = NSTemporaryDirectory()
-
-        // TODO: FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: a, create: true)
-        let url = try XCTUnwrap(NSURL.fileURL(withPathComponents: [tmpDir, "edf-test.edf"]))
-        try "".write(to: url, atomically: true, encoding: .utf8)
-        // TODO: FileManager.default.createFile(atPath: url.absoluteString, contents: nil)
-
-        let startDate = try XCTUnwrap(self.testDate)
-        let birthDate = try XCTUnwrap(self.birthDate)
-
-        let file = FileInformation(
-            subject: .structured(PatientInformation( // TODO: test anonymsous!
-                code: "P-1",
-                sex: .male,
-                birthdate: birthDate,
-                name: "Test Patient" // tests escaping!
-                                                   )),
-            recording: .structured(RecordingInformation(
-                startDate: startDate,
-                code: "R-1",
-                investigatorCode: "I-1",
-                equipmentCode: "Biopot"
-            )),
-            recordDuration: 1
-        )
-
-        let signal = Signal(
-            label: .eeg(location: .af8, prefix: .micro),
-            transducerType: "test-transducer",
-            prefiltering: "test-perfiltering",
-            sampleCount: 5,
-            physicalMinimum: -200,
-            physicalMaximum: 200,
-            digitalMinimum: -250,
-            digitalMaximum: 250,
-            reserved: "This is reserved."
-        )
+    func testEDFPlusHeaderEncoding() throws {
+        let url: URL = try .createTmpFile(name: "edf-test.edf")
+        let startDate: Date = try .createDate(year: 2024, month: 3, day: 4, hour: 18, minute: 51, second: 10)
+        let birthdate: Date = try .createDate(year: 1998, month: 5, day: 26)
 
         let writer = try EDFFileWriter(
             url: url,
-            information: file,
-            signals: [signal]
+            information: .testFile(start: startDate, birthdate: birthdate),
+            signals: [.testEEGSignal]
         )
 
 
         var buffer = ByteBuffer()
-
         writer.encodeHeader(to: &buffer)
 
 
         let string = buffer.readString(length: buffer.readableBytes)
 
-        // TODO: date needs to be upper-cased
-        // TODO: verify spaces!
-        // TODO: digit too much in start date???
-        // TODO: header length is 512 right?
+        let headerLength = 512
+        let recordCount = -1
+
         let expectedHeader =
         """
         .       \
@@ -102,8 +43,8 @@ final class EDFEncodingTests: XCTestCase {
         Startdate 04-Mar-2024 R-1 I-1 Biopot                                            \
         04.03.24\
         18.51.10\
-        512                                                 \
-        -1      \
+        \(headerLength.description)                                                 \
+        \(recordCount.description)      \
         1       \
         1   \
         EEG AF8         \
@@ -118,14 +59,53 @@ final class EDFEncodingTests: XCTestCase {
         This is reserved.               \
 
         """
+        XCTAssertEqual(string, expectedHeader)
+    }
 
-        // ".       P-1 M 01-Mar-2024 Test_Patient
-        // Startdate 01-Mar-02024 R-1 I-1 Biopot
-        // 01.03.2416.45.44512                                                 -1      1       1   EEG AF8
-        // test-transducer
-        // uV      -200    200     -250    250
-        // test-perfiltering
-        // 5       This is reserved.               "
+    func testEDFStartDateY2k() throws {
+        let url: URL = try .createTmpFile(name: "edf-test-y2k.edf")
+        let startDate: Date = try .createDate(year: 2094, month: 3, day: 4, hour: 18, minute: 51, second: 10)
+        let birthdate: Date = try .createDate(year: 1998, month: 5, day: 26)
+
+
+        let writer = try EDFFileWriter(
+            url: url,
+            information: .testFile(start: startDate, birthdate: birthdate),
+            signals: [.testEEGSignal]
+        )
+
+        var buffer = ByteBuffer()
+        writer.encodeHeader(to: &buffer)
+
+
+        let string = buffer.readString(length: buffer.readableBytes)
+
+        let headerLength = 512
+        let recordCount = -1
+
+        let expectedHeader =
+            """
+            .       \
+            P-1 M 26-May-1998 Test_Patient                                                  \
+            Startdate 04-Mar-2094 R-1 I-1 Biopot                                            \
+            04.03.yy\
+            18.51.10\
+            \(headerLength.description)                                                 \
+            \(recordCount.description)      \
+            1       \
+            1   \
+            EEG AF8         \
+            test-transducer                                                                 \
+            uV      \
+            -200    \
+            200     \
+            -250    \
+            250     \
+            test-perfiltering                                                               \
+            5       \
+            This is reserved.               \
+
+            """
         XCTAssertEqual(string, expectedHeader)
     }
 }
