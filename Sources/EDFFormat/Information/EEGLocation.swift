6208f6bc9d7790603ea78d919da82f1e556d48d4
Andreas Bauer <andreas.bauer@stanford.edu>
2024-03-15 11:16:06 -0700

    Add Codable conformance

--- Sources/EDFFormat/Information/EEGLocation.swift
+++ Sources/EDFFormat/Information/EEGLocation.swift
@@ -200,3 +200,18 @@ public struct EEGLocation {
 }
 
 extension EEGLocation: Hashable, Sendable {}
+
+
+extension EEGLocation: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+
+        let rawValue = try container.decode(String.self)
+        self.init(rawValue: rawValue)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+}
